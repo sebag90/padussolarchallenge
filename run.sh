03be75d6b50c365fb4d1e717517fc8a7b812bab0
seba <seba.g90@gmail.com>
2025-05-23 12:08:11 +0000

    docker compose

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 docker run -dit \
-    --env VIRTUAL_HOST=turbosail.it \
-    --env LETSENCRYPT_HOST=turbosail.it \
+    --env VIRTUAL_HOST=turbosail.it,padussolarchallenge.it \
+    --env LETSENCRYPT_HOST=turbosail.it,padussolarchallenge.it \
+    --env LETSENCRYPT_SINGLE_DOMAIN_CERTS=true \
     --restart unless-stopped \
     --network website \
     -v $(pwd)/website:/usr/share/nginx/html \
